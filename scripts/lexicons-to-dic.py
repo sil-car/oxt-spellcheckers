d392109b29bbb39d216826c559b261cadb0ec3a0
Nate Marti <nate_marti@sil.org>
2024-01-22 15:46:28 +0100

    fixes for flak8

--- scripts/lexicons-to-dic.py
+++ scripts/lexicons-to-dic.py
@@ -14,6 +14,12 @@ def main():
     if len(sys.argv) < 2:
         print("Error: Need at least 1 lexicon file.")
         exit(1)
+    elif sys.argv[1] in ['-h', '--help']:
+        print(
+            f"usage: {sys.argv[0]} lexicon.txt [lexicon.txt...]\n\n"
+            "DIC contents are printed to stdout"
+        )
+        exit()
 
     dic_lines = []
     for infile in sys.argv[1:]:
@@ -31,13 +37,13 @@ def main():
             # Add affix markers to entries.
             affixes = set()
             if 'Adjective' in ps:
-                affixes.add("A") # plural prefix
+                affixes.add("A")  # plural prefix
             if 'Noun' in ps:
-                affixes.add("A") # plural prefix
+                affixes.add("A")  # plural prefix
             if 'Verb' in ps:
-                affixes.add("B") # noun-subject prefix
+                affixes.add("B")  # noun-subject prefix
             if wd1 in ['mbï', 'mo', 'âla', 'lo', 'ï']:
-                affixes.add("M") # -mvenî suffix
+                affixes.add("M")  # -mvenî suffix
             if len(affixes) > 0:
                 string = '/'
                 for a in sorted(list(affixes)):
@@ -49,6 +55,7 @@ def main():
     dic_ct = str(len(dic_lines))
     dic_lines.insert(0, dic_ct)
     print("\n".join(dic_lines))
+
 
 if __name__ == '__main__':
     main()
