ea001cf6c0b7976f5cb7a80428f13f29bae90487
Nate Marti <nate_marti@sil.org>
2024-01-22 15:47:07 +0100

    fixes for flake8

--- scripts/tools.py
+++ scripts/tools.py
@@ -36,8 +36,14 @@ class Lexicon():
         return len(self.entries)
 
     def get_output_text(self):
-        # return '\n'.join(sango_sort(list(set(e.as_lexicon_line() for e in self.entries if e.gloss_sg is not None))))
-        return '\n'.join(sango_sort(list(set(e.as_lexicon_line() for e in self.entries.values() if e.gloss_sg is not None))))
+        # return '\n'.join(sango_sort(list(set(e.as_lexicon_line()
+        # for e in self.entries if e.gloss_sg is not None))))
+        return '\n'.join(sango_sort(list(set(
+            e.as_lexicon_line()
+            for e in self.entries.values()
+            if e.gloss_sg is not None)))
+        )
+
 
 class Entry():
     """ Basic details: Sango text, parts of speech; e.g.
@@ -58,22 +64,28 @@ class Entry():
 
 
 def key_from_value(dict, value):
-    # This only works with a 1:1 dictionary; i.e. it only returns the first match.
+    # This only works with a 1:1 dictionary; i.e. it only returns the first
+    # match.
     # return list(dict.keys())[list(dict.values()).index(value)]
     return next((k for k, v in dict.items() if v == value), None)
+
 
 def generate_sorted_set_output(in_set):
     return '\n'.join(sorted(list(in_set), key=str.lower))
 
+
 def eprint(*a):
     print(*a, file=sys.stderr)
+
 
 def print_sorted_set(in_set):
     print(generate_sorted_set_output(in_set))
 
+
 def write_sorted_set(in_set, out_file):
     with out_file.open('w') as f:
         f.write('\n'.join(sorted(list(in_set), key=str.lower)))
+
 
 def raw_words_to_nfd(words):
     nfd_words = set()
@@ -88,9 +100,9 @@ def raw_words_to_nfd(words):
         text_word = re.sub(r'[^\w-]', '', punctuated_word)
 
         # Perform text-based filtering.
-        if len(text_word) == 0: # empty words
+        if len(text_word) == 0:  # empty words
             continue
-        if re.match(r'^[0-9]', text_word): # numbers
+        if re.match(r'^[0-9]', text_word):  # numbers
             continue
 
         nfd_word = unicodedata.normalize('NFD', text_word)
@@ -98,12 +110,13 @@ def raw_words_to_nfd(words):
 
     return nfd_words
 
+
 def sango_sort(in_list):
     def fmt(string):
         return unidecode.unidecode(string.lower())
     return sorted(in_list, key=fmt)
 
+
 def str_to_words(text_string):
     raw_words = text_string.split(' ')
     return raw_words_to_nfd(raw_words)
-
